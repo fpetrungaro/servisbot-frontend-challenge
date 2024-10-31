8d9ee600f04951e1473f90d39204560d2351cdd9
Fabio Petrungaro <fpetrungaro@nasuni.com>
2024-10-31 20:32:29 +0000

    unused import

--- __test__/components/Breadcrumbs.test.tsx
+++ __test__/components/Breadcrumbs.test.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Breadcrumb from '../../components/Breadcrumbs';
-import LogList from "@/components/LogList";
 
 describe('Breadcrumb Component', () => {
   const items  = [
